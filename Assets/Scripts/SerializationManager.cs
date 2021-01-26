2db7e3c992e796ed345a8d8daa70ab0cc0627cd4
Arniel <aceba1@outlook.com>
2021-01-26 00:09:32 -0500

    Placeholder

---
+++ Assets/Scripts/SerializationManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using UnityEngine;
+
+public class SerializationManager : MonoBehaviour
+{
+    public static IEnumerable<Type> FindByDescendant<T>() where T : Attribute
+    {
+        return from assembly in AppDomain.CurrentDomain.GetAssemblies()
+               from type in assembly.GetTypes()
+               where type.IsSubclassOf(typeof(T))
+               select type;
+    }
+
+    static void Foo()
+    {
+        //JsonUtility.FromJson<>("");
+        //JsonUtility.ToJson(, true);
+    }
+}
+
+[Serializable]
+public abstract class Serializer
+{
+
+}
